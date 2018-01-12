e765789e7d2404cb907e0e018bb351711365b8b9
Jonathan Gautheron <alpherz@gmail.com>
2018-01-12 20:26:42 +0100

    Switch from API Gateway to Lambda@Edge

--- middleware.js
+++ middleware.js
@@ -1,22 +1,22 @@
 module.exports.eventContext = options => (req, res, next) => {
     options = options || {} // defaults: {reqPropKey: 'apiGateway', deleteHeaders: true}
-    const reqPropKey = options.reqPropKey || 'apiGateway'
+    const reqPropKey = options.reqPropKey || 'edge'
     const deleteHeaders = options.deleteHeaders === undefined ? true : options.deleteHeaders
 
-    if (!req.headers['x-apigateway-event'] || !req.headers['x-apigateway-context']) {
-        console.error('Missing x-apigateway-event or x-apigateway-context header(s)')
+    if (!req.headers['x-edge-event'] || !req.headers['x-edge-context']) {
+        console.error('Missing x-edge-event or x-edge-context header(s)')
         next()
         return
     }
 
     req[reqPropKey] = {
-        event: JSON.parse(decodeURIComponent(req.headers['x-apigateway-event'])),
-        context: JSON.parse(decodeURIComponent(req.headers['x-apigateway-context']))
+        event: JSON.parse(decodeURIComponent(req.headers['x-edge-event'])),
+        context: JSON.parse(decodeURIComponent(req.headers['x-edge-context']))
     }
 
     if (deleteHeaders) {
-        delete req.headers['x-apigateway-event']
-        delete req.headers['x-apigateway-context']
+        delete req.headers['x-edge-event']
+        delete req.headers['x-edge-context']
     }
 
     next()
